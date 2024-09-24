a1ebd3965b953431dd128facaf4876a30866e9a1
Deepak gupta <deepakgupta70363@gmail.com>
2024-09-24 09:08:17 +0530

    Quiz completed screen added, now once the questions are exhausted the app won't crash

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import questions from "../questions.js";
+import quizCompleteImage from '../assets/quiz-complete.png';
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+
+  
+  const quizIsComplete=activeQuestionIndex===questions.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevAnswers) => {
@@ -11,12 +15,23 @@ export default function Quiz() {
     });
   }
 
+  if(quizIsComplete){
+    return <div id="summary">
+      <img src={quizCompleteImage} alt="Quiz over but image doesn't exist"/>
+      <h2>Quiz completed</h2>
+    </div>
+  }
+  
+  //For presenting options in random order
+  const shuffledAnswers=[...questions[activeQuestionIndex].answers];
+  shuffledAnswers.sort(()=>Math.random()-0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{questions[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {questions[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
